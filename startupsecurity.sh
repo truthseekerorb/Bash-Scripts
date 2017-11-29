73d5318ba91468ae48b5d41c3b687e02e8318211
truthseekerorb <levente.b03@gmail.com>
2017-11-29 16:46:34 -0500

    Create startupsecurity.sh

---
+++ startupsecurity.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#On startup this script checks the startup folder for a password script,
+#runs the script and ask the user for a secondary password before shutting down the computer within 'n' seconds.
+
+if [[ $
